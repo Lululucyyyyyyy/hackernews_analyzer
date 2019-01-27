cd6b1b0a1cdd9188f390b5e032972a259de8ad0a
Lucy <lucy13939@gmail.com>
2019-01-26 19:25:42 -0800

    Finished analyzer hackernews

--- sent_anl.py
+++ sent_anl.py
@@ -2,6 +2,7 @@ import requests
 import os
 import csv
 import time
+import pprint
 
 x = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 y = os.path.realpath('/Users/wenmo/Development/polyglot/cohort-1/data/goodwords.txt')
@@ -25,75 +26,120 @@ def wordlists(path, set):
 
 def display(set):
     for word in set:
-        print (word)
+        #print (word)
+        pass
 
 wordlists(y, goodwords)
 wordlists(z, badwords)
 
-story2 = []
-
 class TweetAnalyzer:
-    def __init__(self, goodwords_list, badwords_list, story2):
+    def __init__(self, goodwords_list, badwords_list):
         self.goodwords = goodwords_list
         self.badwords = badwords_list
-        self.story2 = story2
-        self.comments = self.get_comments(x, story2)
-        self.gnum = 0
-        self.bnum = 0
 
-    def get_comments(self, url, story2):
-        new_response = requests.get(url)
-        top_five_ids = new_response.json()[:100]
+    def get_stories(self):
+        new_response = requests.get(x)
+        top_five_ids = new_response.json()[:10]
 
-        comments = {}
+        story2 = []
         for top_id in top_five_ids:
-            curr_story_api_url = (url)
-            curr_story = requests.get(curr_story_api_url)
-            story2.append(curr_story.json())
-            
+            curr_story_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(top_id)
+            curr_story = requests.get(curr_story_api_url).json()
+            story2.append(curr_story)
+        return story2
+
+    def get_comments(self, story2):
+        story2 = self.get_stories()
+        comments = {}   
         for story in story2:
-            comment_ids = story['kids']
-            title = story['title']
-            comments[title] = [] 
-            for comment_id in comment_ids:
-                curr_id = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(comment_id)
-                curr_comment = requests.get(curr_id).json()['text']
-                comments[title].append(curr_comment)
+            if 'kids' in story:
+                comment_ids = story['kids'][:10]
+                title = story['title']
+                comments[title] = [] 
+                for comment_id in comment_ids:
+                    curr_id = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(comment_id)
+                    curr_comment_obj = requests.get(curr_id).json()
+                    if 'text' in curr_comment_obj:
+                        curr_comment = curr_comment_obj['text']
+                        comments[title].append(curr_comment)
         return comments
     
     def analyze_all_comments(self):
-        for comment in self.comments:
+        print('analyzing comments')
+        story2 = self.get_stories()
+        comments = self.get_comments(story2)
+        for comment in comments:
+            gnum = 0
+            bnum = 0
+            total = 0
             for words in comment:
-                words = comment.split(' ')
+                words = comment.lower().split()
                 for word in words:
                     if word in self.goodwords:
-                        self.gnum += 1
+                        gnum += 1
                     if word in self.badwords:
-                        self.bnum += 1
-            comment_sentiment_pos = gnum / len(comment)
-            comment_sentiment_neg = gnum / len(comment)
-            print(comment +  "{0:.0%}".format(comment_sentiment_pos) + "{0:.0%}".format(comment_sentiment_neg))
+                        bnum += 1
+                    total += 1
+            comment_sentiment_pos = gnum / total
+            comment_sentiment_neg = bnum / total
+            print(comment +  " positive: "+ " {0:.0%}".format(comment_sentiment_pos) + " negative: " + " {0:.0%}".format(comment_sentiment_neg))
 
+    def analyze_comments(self, story):
+        #get comments for one story
+        entire_one_story = {}
+        if 'kids' in story:
+            comment_ids = story['kids'][:10]
+            title = story['title']
+            entire_one_story[title] = []
+            comments = {}
+            comment_text = {}
+            for comment_id in comment_ids:
+                comments[comment_id] = {}
+                curr_id = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(comment_id)
+                curr_comment_obj = requests.get(curr_id).json()
+                sentiment = {}
+                if 'text' in curr_comment_obj:
+                    curr_comment = curr_comment_obj['text']
+                    comment_text['text'] = [curr_comment]
+                    sentiment['sentiment'] = []
+                    for words in curr_comment:
+                        words = curr_comment.lower().split()
+                        for word in words:
+                            total = 0
+                            gnum = 0
+                            bnum = 0
+                            if word in self.goodwords:
+                                gnum += 1
+                            if word in self.badwords:
+                                bnum += 1
+                            total += 1
+                        if gnum > bnum:
+                            sentimentt = 'positive'
+                        elif gnum < bnum:
+                            sentimentt = 'negative'
+                        else:
+                            sentimentt = 'neutral'
+                    sentiment['sentiment'].append(sentimentt)
+                    comments[comment_id].update(comment_text)
+                    comments[comment_id].update(sentiment)
+           
+                entire_one_story[title] = [comments]
+                #return entire_one_story
+        else:
+            pass
 
-    def input_tweet(self):
-        wordnum = 0
-        bnum = 0
-        gnum = 0
-        mytweet = input("the tweet is: ")
-        for word in mytweet.split():
-            if word in self.badwords:
-                bnum += 1
-            if word in self.goodwords:
-                gnum += 1
-            wordnum += 1
-        tweet_sentiment = (gnum - bnum) / wordnum 
+        
 
-        return "{0:.0%}".format(tweet_sentiment)
+    def full_dict(self):
+        stories_list = self.get_stories()
+        my_list = []
+        for story in stories_list:
+            my_list.append(self.analyze_comments(story))
+        #return my_list
 
 if __name__ == "__main__":
-    a1 = TweetAnalyzer(goodwords, badwords, story2)
-    #print("Tweet Sentiment: " + str(a1.input_tweet()))
-    a1.analyze_all_comments()
+    a1 = TweetAnalyzer(goodwords, badwords)
+    print(a1.full_dict())
 
 
 
